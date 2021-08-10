7ed9db99176558315da9110a73be657aa48086b2
Andrew Powers <apowers10@cnm.edu>
2021-08-10 16:03:58 -0400

    matching git to local

--- frontend/src/ui/App.js
+++ frontend/src/ui/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.css';
 import 'mapbox-gl/dist/mapbox-gl.css';
+import {Map} from "./Map";
 
 export function App () {
     return (
         <>
+            <Map/>
         </>
     )
 }
