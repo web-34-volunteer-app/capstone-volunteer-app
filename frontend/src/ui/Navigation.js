341d3a3115c565c373a493878fd28b8bdb4fb7b5
Adriana Martinez <amartinezguzma@cnm.edu>
2021-08-13 09:10:15 -0600

    Added navbar

--- frontend/src/ui/Navigation.js
+++ frontend/src/ui/Navigation.js
@@ -1,43 +1,35 @@
 import React from "react";
-import {Container, Nav, Navbar} from "react-bootstrap";
+import {Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
 import HeaderImage from "./images/HeaderLogoImage.svg";
 
 export function Navigation() {
 
     return (
-        <Navbar bg="dark" variant="dark">
-            <Container className="ms-auto">
+        <Navbar bg="light" expand="lg">
+            <Container>
                 <Navbar.Brand href="#home">
                     <img
                         src={HeaderImage}
-                        width="30"
-                        height="30"
+                        width="100"
+                        height="25"
                         className="d-inline-block align-top"
                         alt="React Bootstrap logo"
                     />
-                    <Nav>
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
-                    </Nav>
                 </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#">Register</Nav.Link>
+                        <Nav.Link href="#">Log In</Nav.Link>
+                        <Nav.Link href="#">Contact</Nav.Link>
+                        <Nav.Link href="#">Log Out</Nav.Link>
+                        <Nav.Link href="#">Privacy Settings</Nav.Link>
+                        <Nav.Link href="#">Community Guidelines</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
             </Container>
         </Navbar>
     )
 
 }
 
-        /*<Navbar bg="dark" variant="dark">
-            <Container>
-                <Navbar.Brand href="#home">
-                    <img
-                        alt=""
-                        src={HeaderImage}
-                        width="100"
-                        height="100"
-                        className="d-inline-block align-top"
-                    />
-                    React Bootstrap
-                </Navbar.Brand>
-            </Container>
-        </Navbar>*/
