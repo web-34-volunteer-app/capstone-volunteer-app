1ac2af70347ca678f4ffea232be196ab1677fd97
Adriana Martinez <amartinezguzma@cnm.edu>
2021-08-15 15:10:26 -0600

    Removed unused imports

--- frontend/src/ui/UserProfile.js
+++ frontend/src/ui/UserProfile.js
@@ -1,9 +1,6 @@
 import React from "react";
-import {Container} from "react-bootstrap";
 import {UserOverview} from "./UserOverview";
-import {Map} from "./Map";
-import {CreateEventForm} from "./CreateEventForm";
-import {EventDetails} from "./EventDetails";
+
 
 export function UserProfile () {
     return (
