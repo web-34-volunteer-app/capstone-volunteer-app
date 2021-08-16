c10ac9e07824fbde623a67d290b0f703a6bf2475
Adriana Martinez <amartinezguzma@cnm.edu>
2021-08-15 20:29:53 -0600

    Created event form component under CreateEventForm.js. Component has some styling and tentative information needed to create event.

--- frontend/src/ui/CreateEventForm.js
+++ frontend/src/ui/CreateEventForm.js
@@ -1,25 +1,40 @@
 import React from "react";
-import {Container, Row} from "react-bootstrap";
+import {Col, Row} from "react-bootstrap";
+import {Form} from "react-bootstrap";
+import {Button} from "react-bootstrap";
 
 export function CreateEventForm() {
     return(
         <>
-        <Container>
-           <Row>
-               <h1>Event Details</h1>
-           </Row>
-            <Row>
-            </Row>
-            <Row>
+            <Form>
+                <Row className={"p-2"}>
+                    <Form.Label column="lg" className="bg-dark text-white">
+                        Event Information
+                    </Form.Label>
+                </Row>
+                <Row className={"mt-3"}>
+                    <Col>
+                        <Form.Control size="text" type="text" placeholder="Event Name"/>
+                    </Col>
+                    <Col>
+                        <Form.Control size="text" type="text" placeholder="Organization"/>
+                    </Col>
+                </Row>
+                <Row className={"mt-3"}>
+                    <Col>
+                        <Form.Control type="date"/>
+                    </Col>
+                    <Col>
+                        <Form.Control type="time"/>
+                    </Col>
+                </Row>
 
-            </Row>
-            <Row>
-
-            </Row>
-            <Row>
-
-            </Row>
-        </Container>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                    <Form.Label/>
+                    <Form.Control as="textarea" placeholder="Event Description" rows={3}/>
+                </Form.Group>
+                <Button variant="outline-success" className={"align-content-center"}>Create</Button>
+            </Form>
         </>
     )
 
