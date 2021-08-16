e957f4645c4e2ce8fa4e7497d45e42aa763e5a45
Adriana Martinez <amartinezguzma@cnm.edu>
2021-08-16 11:16:01 -0600

    Created example of props on event details card.

--- frontend/src/ui/EventDetails.js
+++ frontend/src/ui/EventDetails.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {Button, Card} from "react-bootstrap";
 
-export function EventDetails() {
+export function EventDetails(props) {
     return(
         <>
             <Card
@@ -10,11 +10,9 @@ export function EventDetails() {
                 style={{ width: "auto"}}
                 className="m-3">
                 <Card.Body>
-                    <Card.Title>Event Name</Card.Title>
-                    <Card.Subtitle className="mb-2 text-muted">Point of Contact</Card.Subtitle>
-                    <Card.Text>
-                        (Event Description) Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                    </Card.Text>
+                    <Card.Title>{props.eventName}</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">{props.eventName}</Card.Subtitle>
+                    <Card.Text>{props.eventDescription}</Card.Text>
                     <Button variant="outline-success">Register</Button>
                     <Button variant="outline-danger">Report</Button>
                 </Card.Body>
--- frontend/src/ui/UserProfile.js
+++ frontend/src/ui/UserProfile.js
@@ -1,6 +1,31 @@
 import React from "react";
 import {UserOverview} from "./UserOverview";
 import {EventApprovalTable} from "./EventApprovalTable";
+import {CreateEventForm} from "./CreateEventForm";
+import {EventDetails} from "./EventDetails";
+
+
+let events = [
+    {
+        id: 0,
+        eventName: 'Save a Pet',
+        pointOfContact: 'Andrew',
+        eventDescription: 'Blah blah blah'
+    },
+    {
+        id: 1,
+        eventName: 'Save a Bird',
+        pointOfContact: 'Jericho',
+        eventDescription: 'Blah blah blah'
+    }
+]
+
+let eventDetails = []
+
+events.forEach((event, index) =>{
+    eventDetails.push(<EventDetails eventName={event.eventName} pointOfContact={event.pointOfContact} eventDescription={event.eventDescription}/>)
+
+})
 
 
 export function UserProfile () {
@@ -8,6 +33,8 @@ export function UserProfile () {
         <>
             <UserOverview/>
             <EventApprovalTable/>
+            <CreateEventForm/>
+            {eventDetails}
         </>
     )
 }
