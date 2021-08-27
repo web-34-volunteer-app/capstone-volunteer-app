0eb5e7e77126a8f9af6e05baafc25072e82419bf
Jericho Ortiz <jortiz180@cnm.edu>
2021-08-27 10:50:40 -0600

    created volunteer tables

--- sql/project.sql
+++ sql/project.sql
@@ -1,40 +1,24 @@
-drop table if exists articleTag;
-drop table if exists article;
-drop table if exists tag;
-drop table if exists author;
-create table author(
-                       authorId binary(16) not null,
-                       authorActivationToken char(32),
-                       authorAvatarUrl varchar(255),
-                       authorEmail varchar(128) not null,
-                       authorHash char(97) not null,
-                       authorUsername varchar(32) not null,
-                       unique(authorEmail),
-                       unique(authorUsername),
-                       index(authorEmail),
-                       primary key(authorId)
+CREATE TABLE event(
+                    eventId BINARY(16) NOT NULL,
+                    PRIMARY KEY (eventId)
 );
-create table tag(
-                    tagId binary(16) not null,
-                    tagName varchar(32) not null,
-                    primary key(tagId)
+
+
+
+CREATE TABLE user(
+                    userId BINARY(16) NOT NULL,
+                    PRIMARY KEY (userId)
 );
-create table article(
-                        articleId binary(16) not null,
-                        articleAuthorId binary(16) not null,
-                        articleContent blob not null,
-                        articleDate datetime(6) not null,
-                        articleImage varchar(255),
-                        index(articleAuthorId),
-                        foreign key(articleAuthorId) references author(authorId),
-                        primary key(articleId)
-);
-create table articleTag(
-                           articleTagArticleId binary(16),
-                           articleTagTagId binary(16),
-                           index(articleTagArticleId),
-                           index(articleTagTagId),
-                           foreign key(articleTagArticleId) references article(articleId),
-                           foreign key(articleTagTagId) references tag(tagId),
-                           primary key(articleTagArticleId, articleTagTagId)
+
+
+CREATE TABLE volunteer(
+                        volunteerEventId BINARY(16) NOT NULL,
+                        volunteerUserId BINARY(16) NOT NULL,
+                        volunteerHours DECIMAL(6,2),
+                        volunteerHoursPosterVerified BOOLEAN,
+                        volunteerHoursVolunteerVerified BOOLEAN,
+                        INDEX (volunteerEventId),
+                        INDEX (volunteerUserId),
+                        FOREIGN KEY (volunteerEventId) REFERENCES event(eventId),
+                        FOREIGN KEY (volunteerUserId) REFERENCES user(userId)
 );
