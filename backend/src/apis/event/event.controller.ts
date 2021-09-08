81ff0fbc4c1b7e746b4089cd089dd6242c410be1
Adriana Martinez <amartinezguzma@cnm.edu>
2021-09-08 16:23:33 -0600

    Started updateEvent function

--- backend/src/apis/event/event.controller.ts
+++ backend/src/apis/event/event.controller.ts
@@ -5,13 +5,22 @@ import {User} from "../../utils/interfaces/User";
 import {insertEvent} from "../../utils/event/insertEvent";
 
 
-
 const {validationResult} = require('express-validator');
 
-export async function postEvent(request:Request, response:Response){
+export async function postEvent(request: Request, response: Response) {
     try {
-        console.log("request.body",request.body)
-        const {eventAddress, eventDate, eventDescription, eventDescriptionSkillsRequired,eventDescriptionTransportation, eventDescriptionTypeOfWork, eventEndTime, eventOrganization,eventStartTime} = request.body;
+        console.log("request.body", request.body)
+        const {
+            eventAddress,
+            eventDate,
+            eventDescription,
+            eventDescriptionSkillsRequired,
+            eventDescriptionTransportation,
+            eventDescriptionTypeOfWork,
+            eventEndTime,
+            eventOrganization,
+            eventStartTime
+        } = request.body;
 
         const eventUserId = <string>request.session?.user?.userId
 
@@ -38,14 +47,73 @@ export async function postEvent(request:Request, response:Response){
 
         const result = await insertEvent(event)
         const status: Status = {
-            status:200,
+            status: 200,
             message: result ?? 'Event created successfully',
-            data:null
+            data: null
         };
         return response.json(status);
 
-    }catch (error){
+    } catch (error) {
         console.log(error)
     }
 
 }
+
+
+//Incomplete attempt to add controller
+
+export async function putEventController(request: Request, response: Response): Promise<Response> {
+    try {
+        const {eventId} = request.params
+        const {
+            eventAddress,
+            eventDate,
+            eventDescription,
+            eventDescriptionSkillsRequired,
+            eventDescriptionTransportation,
+            eventDescriptionTypeOfWork,
+            eventEndTime,
+            eventFlag,
+            eventLatitude,
+            eventLongitude,
+            eventOrganization,
+            eventStartTime
+        } = request.body
+
+        const previousEvent = await selectEventByEventId(eventId)
+        const preformUpdate = async (thisEvent: Event): Promise<Response> => {
+            const newEvent: Event = {...previousEvent, ...thisEvent}
+            for (let key in newEvent) {
+                //@ts-ignore
+                newEvent[key] = thisEvent[key] ?? previousEvent[key]
+            }
+            await updateEvent(newEvent)
+            return response.json({status: 200, data: null, message: "Event successfully updated"})
+        }
+
+        const updateFailed = (message: string): Response => {
+            return response.json({status: 400, data: null, message})
+        }
+
+        let pass = true
+        return pass
+            ? preformUpdate({
+                eventAddress,
+                eventDate,
+                eventDescription,
+                eventDescriptionSkillsRequired,
+                eventDescriptionTransportation,
+                eventDescriptionTypeOfWork,
+                eventEndTime,
+                eventFlag,
+                eventLatitude,
+                eventLongitude,
+                eventOrganization,
+                eventStartTime
+            })
+            : updateFailed("you are not allowed to preform this action")
+    } catch (error) {
+        return response.json({status: 400, data: null, message: error.message})
+    }
+}
+
---
+++ backend/src/utils/event/updateEvent.ts
@@ -0,0 +1,15 @@
+//Pending testing
+
+import {connect} from '../database.utils';
+import {Event} from "../interfaces/Event";
+
+export async function updateEvent(event: Event): Promise<string> {
+    try {
+        const mysqlConnection = await connect();
+        const query : string = 'UPDATE event SET eventAddress = :eventAddress, eventDate = :eventDate, eventDescription = :eventDescription, eventDescriptionSkillsRequired = :eventDescriptionSkillsRequired, eventDescriptionTransportation = :eventDescriptionTransportation, eventDescriptionTypeOfWork = :eventDescriptionTypeOfWork, eventEndTime = :eventEndTime, eventFlag = :eventFlag, eventLatitude = :eventLatitude, eventLongitude = :eventLongitude, eventOrganization = :eventOrganization, eventStartTime= :eventStartTime WHERE eventId = UUID_TO_BIN(:eventId)';
+        await mysqlConnection.execute(query, event)
+        return 'Event successfully updated'
+    } catch (error) {
+        throw error
+    }
+}
