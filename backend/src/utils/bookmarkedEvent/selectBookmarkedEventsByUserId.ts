bf91da21ccea600aa654d54a9978ae237eace41b
Adriana Martinez <amartinezguzma@cnm.edu>
2021-09-13 14:51:30 -0600

    Created & successfully tested function to select events bookmarked by a user

--- backend/src/utils/bookmarkedEvent/selectBookmarkedEventsByUserId.ts
+++ backend/src/utils/bookmarkedEvent/selectBookmarkedEventsByUserId.ts
@@ -1,13 +1,17 @@
 import {connect} from "../database.utils";
-import {BookmarkedEvent} from "../interfaces/BookmarkedEvent";
+import {RowDataPacket} from "mysql2";
+import {Event} from "../interfaces/Event";
 
-export async function selectBookmarkedEventsByUserId(bookmarkedEventUserId: string) : Promise<Array<BookmarkedEvent|null>> {
+export async function selectBookmarkedEventsByUserId(userId: string) : Promise<Event[]> {
     try {
+        console.log('This is the user Id',userId)
         const mySqlConnection = await connect();
-        const mySqlQuery = 'SELECT BIN_TO_UUID(bookmarkedEventEventId) as bookmarkedEventEventId FROM bookmarkedEvent WHERE bookmarkedEventUserId = :bookmarkedEventUserId';
-        const [rows] = await mySqlConnection.execute(mySqlQuery);
-        return rows;
+        const mySqlQuery = "SELECT BIN_TO_UUID(eventId) AS eventId, BIN_TO_UUID (eventUserId) AS eventUserId, eventAddress, eventDate, eventDescription, eventDescriptionSkillsRequired, eventDescriptionTransportation, eventDescriptionTypeOfWork, eventEndTime, eventFlag, eventLatitude, eventLongitude, eventOrganization, eventStartTime, eventTitle from event inner join bookmarkedEvent on bookmarkedEvent.bookmarkedEventEventId = event.eventId WHERE bookmarkedEvent.bookmarkedEventUserId = UUID_TO_BIN(:userId)"
+
+        const result = await mySqlConnection.execute(mySqlQuery, {userId}) as RowDataPacket[]
+        const events: Array<Event> = result[0] as Array<Event>
+        return events;
     } catch (error) {
-        console.error(error);
+       throw error
     }
 }
